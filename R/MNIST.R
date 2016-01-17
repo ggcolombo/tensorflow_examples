0926e190432ff6981d0edd9ad2515df5e5f1786c
Luigi <luigi.colombo@smartodds.co.uk>
2016-01-17 22:31:16 +0000

    Use first 1000 rows to fit multinom

--- R/MNIST.R
+++ R/MNIST.R
@@ -5,12 +5,17 @@ if (!file.exists('./MNIST_data/train.RData')){
 	readMNIST(folder='./MNIST_data/')
 }
 load('./MNIST_data/train.RData')
+load('./MNIST_data/test.RData')
 
 # Visualise the first digit - flipping the upside-down image
 image(matrix(trainData[1,], nrow=28, ncol=28)[,28:1])
 # Read the true value
 which(as.logical(trainLabels[1,]))-1
 
+y <- head(trainLabels, 1e3)
+x <- head(trainData, 1e3)
+mod <- multinom(y ~ x, MaxNWts = 7860)
 
-mod <- multinom(trainLabels ~ trainData)
+pred <- predict(mod, newdata=testData)
+
  
